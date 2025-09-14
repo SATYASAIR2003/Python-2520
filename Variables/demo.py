7651bbd82c29b8f3b514cca659375277508ba9ae
SATYASAIR2003 <raavisaikondayya@gmail.com>
2025-09-14 10:55:36 +0530

    Input

---
+++ Variables/demo.py
@@ -0,0 +1,13 @@
+# print("good")
+# if(2<5):
+#     print("yes")
+# print("yoswe")
+
+name = input("Enter your name ")
+print(name)
+
+age = int(input("enter your age "))
+if age<18:
+    print("Teenager")
+else:
+    print("Adult")
