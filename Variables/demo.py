ecf04e8d31061d9a37edc03c855fd8a8c9a8c0bb
SATYASAIR2003 <raavisaikondayya@gmail.com>
2025-09-14 11:03:41 +0530

    type casting

--- Variables/demo.py
+++ Variables/demo.py
@@ -3,11 +3,15 @@
 #     print("yes")
 # print("yoswe")
 
-name = input("Enter your name ")
-print(name)
+# name = input("Enter your name ")
+# print(name)
 
-age = int(input("enter your age "))
-if age<18:
-    print("Teenager")
-else:
-    print("Adult")
+# age = int(input("enter your age "))
+# if age<18:
+#     print("Teenager")
+# else:
+#     print("Adult")
+
+data=3.92
+data=int(data)
+print(data)
