27c8a90bfb227573fa4229cd3468d7ade7185b5e
SATYASAIR2003 <raavisaikondayya@gmail.com>
2025-09-17 14:59:52 +0530

    Task Complete

--- Variables/Task/Task1.py
+++ Variables/Task/Task1.py
@@ -1,6 +1,8 @@
-Student_Id = int(input("Enter your Id: "))
+Student_Id = (input("Enter your Id: "))
 Student_Name=input("Enter your Name: ")
 Attendance=int(input("Enter your Attendance: "))
+
+# Ask Student To Enter Score / Marks
 count=1
 marks={}
 while True:
@@ -12,7 +14,43 @@ while True:
     else:
         print(marks)
         break
+
+# Give me the number of scores
 values=list(marks.values())
-print(len(values[:]))
+num=len(values)
+print(num)
+
+# Calculate Total Score (Score 1 + Score 2 + .... + Score N )
+Total=0
+for n in values:
+    Total = Total+n 
+
+# Give me the Average score
+average=Total/num
+print("Total Marks: ", Total)
+print("Average Marks: ", average)
+
+# Based On Average Score, Grade the Student
+
+if(average>=85):
+    print("Grade A")
+elif(average>=70):
+    print("Grade B")
+elif(average>=50):
+    print("Grade C")
+else:
+    print("Fail")
+    
+# Attendance Criteria
+if (Attendance<75):
+    print("Warning Low Attendance")
+else:
+    print("Ok Good Attendance")
+
+# Award Eligibility 
+if(Attendance>=75 and average>=85 ):
+    print("Eligible for Award")
+else:
+    print("Not Eligible for Award")
 
     
