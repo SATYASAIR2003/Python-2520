3216e12c208afbdac6ae6fca61846daef81e24c9
SATYASAIR2003 <raavisaikondayya@gmail.com>
2025-10-23 11:13:33 +0530

    23-10

---
+++ oops/demo.py
@@ -0,0 +1,24 @@
+# Student class
+
+class Student:
+
+    # class variable -> class date common all students 
+    institute_name = "DigitalEdify"
+
+    # instance data specific to each student 
+    def __init__(self,student_id,student_name,student_age,student_email,student_mobile):
+        # identity info
+        self.student_id= student_id
+        self.student_name=student_name
+        self.student_age=student_age
+        self.student_email=student_email
+        self.student_mobile=student_mobile
+
+        # calculation info
+        
+    
+
+
+
+
+
