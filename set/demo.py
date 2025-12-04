c07ef04b523e065283d64369e2e04331c0387a68
SATYASAIR2003 <raavisaikondayya@gmail.com>
2025-12-04 11:45:47 +0530

    4122025

--- set/demo.py
+++ set/demo.py
@@ -38,14 +38,38 @@
 # a=(1 + 2j) * (3 + 4j)
 # print(a)
 
-a=[10,20]
-b=["Satya","Sai"]
-for i in a:
-    for j in b:
-        print(i,j)
+# a=[10,20]
+# b=["Satya","Sai"]
+# for i in a:
+#     for j in b:
+#         print(i,j)
 
-# True = False
-# while True:
-#     print(True)
-#     break
-# SyntaxError: cannot assign to True
+# # True = False
+# # while True:
+# #     print(True)
+# #     break
+# # SyntaxError: cannot assign to True
+
+# li = ['a', 'b', 'c', 'd']
+# print("".join(li))
+
+# def func(a, b=[]):
+#     b.append(a)
+#     return b
+
+# print(func(1))
+# print(func(2))
+
+def REVERSE(a): 
+	a.reverse() 
+	return(a)
+print(REVERSE([2,3,4,5]))
+
+def YKNJS(a): 
+	b = [] 
+	b.extend(REVERSE(a)) 
+	print(b)
+print(YKNJS([2,3,5]))
+
+li = ['a', 'b', 'c'] * -3
+print(li)
