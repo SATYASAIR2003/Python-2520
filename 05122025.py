bf506189688b1089951f7173795e45d8568824fa
SATYASAIR2003 <raavisaikondayya@gmail.com>
2025-12-06 00:35:10 +0530

    6122025

---
+++ 05122025.py
@@ -0,0 +1,35 @@
+# a = "Satya"
+# b = int(a)
+# print(a)
+# print(b)
+# ValueError: invalid literal for int() with base 10: 'Satya'
+
+# a="Satya"
+# del a
+# print("deleted")
+
+# print(a)
+# NameError: name 'a' is not defined 
+# because the value is deleted using "del" keyword
+
+# a = "satya sass"
+# print(a)
+# a= "S"+a[1:]
+# print(a)
+# b=a.replace("sass","Sai") 
+# print(len(b))
+
+# d="satya"
+# print(d.upper())
+
+# e="saty sai asd"
+# print(e.title())
+
+a= "Satya"
+b="Sai"
+c = 23
+print(f"My name is {a} {b} and age is {c}")
+print(f"{a}")
+print(f"{{satya}}")
+print(f"{{{a}}}")
+print(f"{{{{Sstya}}}}")
