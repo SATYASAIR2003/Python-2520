7ee42431dafc165d56a29e26a352edb6b4e25778
SATYASAIR2003 <raavisaikondayya@gmail.com>
2025-12-23 00:52:04 +0530

    taking a string and getting all possible sub strings and from that get substrings of length k and give the count of those strings

--- Problems/slidingwindow.py
+++ Problems/slidingwindow.py
@@ -4,26 +4,27 @@
 #     for j in range(i,k+1):
 #         max()
 
-a=[5,9,1,8,7]
-h=3
-n=len(a)
-ans=[]
-for i in range(n):
-    for j in range(i,n):
-        an=[]
-        for k in range(i,j+1):
-            an.append(a[k])
-        # print(an)
-        ans.append(an)
-print(ans)
-max1=[]
-for i in ans:
-    if len(i)==h:
-        sum1=0
-        for k in i:
-            sum1=sum1+k
-        max1.append(sum1)
-print(max(max1))
+# a=[5,9,1,8,7]
+# h=3
+# n=len(a)
+# ans=[]
+# for i in range(n):
+#     for j in range(i,n):
+#         an=[]
+#         for k in range(i,j+1):
+#             an.append(a[k])
+#         # print(an)
+#         ans.append(an)
+# print(ans)
+# max1=[]
+# for i in ans:
+#     if len(i)==h:
+#         sum1=0
+#         for k in i:
+#             sum1=sum1+k
+#         max1.append(sum1)
+# print(max(max1))
+
 # a="SAI"
 # n=len(a)
 # an=[]
@@ -35,3 +36,20 @@ print(max(max1))
 #         # print(tem)
 #         an.append(tem)
 # print(an)
+
+a=input("Enter: ")
+n=len(a)
+l=0
+h=3
+b=[]
+count=0
+for i in range(n):
+    for k in range(i,n):
+        tem=""
+        # print(i,k)
+        for r in range(i,k+1):
+            tem+=a[r]
+        if len(tem)==h and len(tem)==len(set(tem)):
+            print(tem)
+            count+=1
+print(count)
